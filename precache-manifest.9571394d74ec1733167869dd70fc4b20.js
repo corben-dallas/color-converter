7807e6dd92fa039ca2e8fa384beac723af3c7b06
albert_u <albert.u1994@gmail.com>
2020-09-10 17:53:45 +0300

    Updates

---
+++ precache-manifest.9571394d74ec1733167869dd70fc4b20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb897911341841c571cafb3ae386a97",
+    "url": "/color-converter/index.html"
+  },
+  {
+    "revision": "2e6a143b0d5873b52e16",
+    "url": "/color-converter/static/css/main.de2456d9.chunk.css"
+  },
+  {
+    "revision": "b82d5be308ea8a0149fc",
+    "url": "/color-converter/static/js/2.6ea30d57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-converter/static/js/2.6ea30d57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6a143b0d5873b52e16",
+    "url": "/color-converter/static/js/main.09aa22dd.chunk.js"
+  },
+  {
+    "revision": "8407a5cc537c36fb771e",
+    "url": "/color-converter/static/js/runtime-main.c444dede.js"
+  }
+]);
